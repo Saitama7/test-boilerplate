af9411361aa3efec180480c534330b5898fc467c
Dj. Kaira <djkaira7@gmail.com>
2019-10-28 15:13:03 +0600

    local

--- src/config.js
+++ src/config.js
@@ -6,17 +6,17 @@ const config = {
     isDev: process.env.NODE_ENV !== 'production',
     basename: process.env.PUBLIC_PATH,
     isBrowser: typeof window !== 'undefined',
-    apiUrl: 'http://157.230.122.15:9000/api/v1/',
+    apiUrl: 'http://localhost:3000',
     fbAppId: '991453140998882',
     googleClientId: '464712936089-q953apdu1bjiqtcjndktnnk1ts4f2cgv.apps.googleusercontent.com',
     gtmId: 'GTM-WX5ZNVC',
   },
   test: {},
   development: {
-    apiUrl: 'http://157.230.122.15:9000/api/v1/',
+    apiUrl: 'http://localhost:3000',
   },
   production: {
-    apiUrl: 'http://157.230.122.15:9000/api/v1/',
+    apiUrl: 'http://localhost:3000',
   },
 }
 
